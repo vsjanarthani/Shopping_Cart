769e8d5f0f4a41efbe98d46b4c9afce20a9fe0ca
jvs <vs.janarthani@gmail.com>
2022-01-23 13:58:33 +0530

    added fetch function to fetch fake data from fakestore api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import { useState} from 'react';
+import { useQuery} from 'react-query';
 
-function App() {
+// Componenets
+import { LinearProgress, Drawer, Grid, Badge } from '@material-ui/core';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+
+// Styles
+import {Wrapper} from './App.styles';
+
+// Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: string;
+}
+
+// Fetching from fakestore API
+
+const getProductData = async (): Promise<CartItemType[]> => 
+await(await fetch('https://fakestoreapi.com/products')).json();
+
+const App = () => {
+  const {data, isLoading, error} = useQuery<CartItemType[]>(
+    'products',
+    getProductData
+  )
+  console.log(data);
   return (
     <div className="App">
     <h1>Getting Started</h1>
